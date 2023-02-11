a88f72567f30de1f6269dd166126120337e1a47d
HARISH-MADDMAXX <fwdtoharish@gmail.com>
2023-02-11 16:23:24 +0530

    1st commit

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,42 @@
+<template>
+<Transition name="modal-outer">
+  <div  
+v-show ="modalActive"
+
+ class="position-absolute text-center mt-2 bg-light ms-5 justify-content-center p-2">
+<Transition name="modal-inner" >
+<div v-if="modalActive" class="p-2 bg-light ">
+
+
+
+<slot />
+<button type="button" class="btn btn-outline-info"
+@click="$emit('close-modal')"
+>close</button>
+
+</div>
+</Transition>
+
+
+</div>
+
+
+
+</Transition>
+
+</template>
+
+<script setup>
+
+defineEmits(['close-modal'])
+
+defineProps({
+  modalActive :{
+    type :Boolean,
+    default:false,
+  },
+})
+
+</script>
+
+
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,93 @@
+<template>
+  <header class="position-sticky shadow  mb-5 bg-body rounded-3">
+    <nav class="container-fluid bg-dark opacity-75 mt-2 rounded-3">
+      <div class="row mx-auto">
+
+        <div class="col">
+
+          <RouterLink :to="{ name: 'home' }" style="text-decoration:none">
+
+            <div class="d-inline-flex align-items-center">
+              <div class="flex-shrink-0">
+                <i class="fa-solid fa-sun fa-2xl text-info" style="position: relative; left:30%"></i>
+              </div>
+              <div class="flex-grow-1 ms-3 mt-1">
+                <p class="text-info mt-2 fs-3">Local weather</p>
+
+              </div>
+
+            </div>
+          </RouterLink>
+        </div>
+        <div class="col">
+          <div class="d-flex flex-row-reverse">
+            <i class="fa-sharp fa-solid fa-circle-info fa-xl text-info mt-4 ms-2" @click="toggleModal"></i>
+           
+              <i class="fa-solid fa-plus fa-xl text-info mt-4 ms-2"></i>
+        
+           
+          </div>
+        </div>
+
+
+
+      </div>
+      <BaseModal :modalActive = "modalActive" @close-modal="toggleModal">
+
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">About:</h1>
+          <p class="mb-4">
+            The Local Weather allows you to track the current and
+            future weather of cities of your choosing.
+          </p>
+          <h2 class="text-2xl">How it works:</h2>
+         <div class="text-start">
+         
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+              Search for your city by entering the name into the
+              search bar.
+            </li>
+            <li>
+              Select a city within the results, this will take
+              you to the current weather for your selection.
+            </li>
+            <li>
+              Track the city by clicking on the "+" icon in the
+              top right. This will save the city to view at a
+              later time on the home page.
+            </li>
+          </ol>
+         
+         </div>
+
+          <h2 class="text-2xl">Removing a city</h2>
+          <p>
+            If you no longer wish to track a city, simply select
+            the city within the home page. At the bottom of the
+            page, there will be am option to delete the city.
+          </p>
+        </div> 
+
+ 
+      </BaseModal>
+
+    </nav>
+
+  </header>
+</template>
+
+<script setup>
+
+import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+
+import BaseModal from './BaseModal.vue';
+
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value
+}
+</script>
+
+
